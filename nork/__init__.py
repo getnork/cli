0e9f36765ae4913cee582636bf1a8ad2ffe93aa1
NOR/K <support@nork.sh>
2021-06-29 22:00:31 +0000

    install

--- nork/__init__.py
+++ nork/__init__.py
@@ -11,12 +11,11 @@ $$ |  $$ |\$$$$$$  |$$ |      $$ | \$$\
 from nork.core import paths
 import toml
 
-__version__ = "0.1.10"
+__version__ = "0.1.12"
 
 config = dict()
 
 try:
-    project_root = paths.COMMANDS_PATH
-    config = toml.load(f="{project_root}/nork.toml")
+    config = toml.load(f=f"{paths.PROJECT_PATH}/nork.toml")
 except Exception as exception:
     pass
